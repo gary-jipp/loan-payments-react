f10a26ff2a21e7b98a3bc9c673fe4ef6bc9fab69
Gary Jipp <37557999+gary-jipp@users.noreply.github.com>
2021-02-13 20:08:35 -0800

    Add name and exit button

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import axios from 'axios';
 import './App.css';
 
 const App = function () {
-
   const [data, setData] = useState([]);
-  const [uid, setUid] = useState();
+  const [uid, setUid] = useState("");
 
   const onChange = function (event) {
     setUid(event.target.value);
   };
 
-  const onClick = function (event) {
+  const onSubmit = function (event) {
     event.preventDefault();
     const url = `/api/payments/${uid}`;
     axios.get(url).then(res => setData(res.data))
       .catch(e => console.log(e.ernno));
+  };
+
+  const reset = function (event) {
+    setData([]);
+    setUid("");
   };
 
   const itemList = data.map(item =>
@@ -29,36 +33,47 @@ const App = function () {
   );
 
   return (
-    <div className="App">
+    <>
+      {!data.length &&
+        <div className="container">
+          <form onSubmit={onSubmit}>
+            <div className="input-field">
+              <input type="text" id="account-id" required value={uid} onChange={onChange} />
+              <label className="account-id">Account ID</label>
+            </div>
+            <button className="btn blue darken-2 z-depth-0">Enter</button>
+          </form>
+        </div>
+      }
 
-      <div className="container">
-        <form onSubmit={onClick}>
-          <div className="input-field">
-            <input type="text" id="account-id" required value={uid} onChange={onChange} />
-            <label className="account-id">Account ID</label>
+      {data.length > 0 &&
+        <>
+          <div className="container row valign-wrapper">
+            <span className="flow-text">Connected as: {data[0].name}</span>
+            <span className="col s5"></span>
+            <button onClick={reset} className="btn blue darken-2 z-depth-0 right">Exit</button>
           </div>
-          <button className="btn blue darken-2 z-depth-0">Enter</button>
-        </form>
-      </div>
 
-      <div className="container">
-        <table className="highlight">
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Interest</th>
-              <th>Loan</th>
-              <th>Paid</th>
-              <th>Balance</th>
-            </tr>
-          </thead>
-          <tbody>
-            {itemList}
-          </tbody>
-        </table>
-      </div>
+          <div className="container">
+            <table className="highlight">
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Interest</th>
+                  <th>Loan</th>
+                  <th>Paid</th>
+                  <th>Balance</th>
+                </tr>
+              </thead>
+              <tbody>
+                {itemList}
+              </tbody>
+            </table>
+          </div>
+        </>
+      }
 
-    </div >
+    </>
   );
 };
 export default App;
